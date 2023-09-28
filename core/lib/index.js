b47382f93bf3bf451972900e0a3a244a3a1519d3
Tyler Aldrich <tyler.aldrich@architect.io>
2023-09-28 14:32:41 -0400

    Expose websocket endpoint to stream back progress and results for verbose logging

---
+++ core/lib/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BasePlugin = void 0;
+var base_plugin_1 = require("./base-plugin");
+Object.defineProperty(exports, "BasePlugin", { enumerable: true, get: function () { return base_plugin_1.BasePlugin; } });

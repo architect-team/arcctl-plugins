39cddd25c83695887a59b8e8b0500a400bed8e5d
Tyler Aldrich <tyler.aldrich@architect.io>
2023-10-03 16:12:21 -0400

    Remove gitignored lib

--- core/lib/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BasePlugin = void 0;
-var base_plugin_1 = require("./base-plugin");
-Object.defineProperty(exports, "BasePlugin", { enumerable: true, get: function () { return base_plugin_1.BasePlugin; } });
